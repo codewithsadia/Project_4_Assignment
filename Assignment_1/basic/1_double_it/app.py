cdea7a2559d09d82007b7c87a4ae0c9aa4c3394e
sadiaimran <sadiaimranmalik24@gmail.com>
2025-03-23 12:07:32 +0500

    1_double_it is done

---
+++ Assignment_1/basic/1_double_it/app.py
@@ -0,0 +1,5 @@
+curr_value = int(input("Enter a number: "))
+
+while curr_value < 100:
+    curr_value *= 2  # Double the current value
+    print(curr_value, end=" ")  # Print the value in the same line
